0cc40803dda08a1bef91073dad8223365961ca19
federus rudi <federusrudi@gmail.com>
2025-08-10 17:17:55 +0700

    feat: styling on screen <1024

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div className=" bg-white">
-        <footer className="grid grid-cols-4 place-items-center pl-24 pr-27">
-          <section className="left">
+      <div className=" bg-white max-lg:px-4">
+        <footer className="grid grid-cols-4 place-items-center pl-24 pr-27 max-lg:flex max-lg:flex-col max-lg:mt-10 max-lg:items-start">
+          <section className="pt-5">
             <img src="/src/public/Tickitz 1.png" alt="" />
             <p className="text-gray-400">
               Stop waiting in line. Buy tickets
@@ -14,19 +14,19 @@ function Footer() {
             </p>
           </section>
           <section id="left2" className="flex flex-col gap-2.5">
-            <h1>Explore</h1>
+            <h1 className="font-bold max-lg:pt-3.5">Explore</h1>
             <p className="text-gray-400">Cinemas</p>
             <p className="text-gray-400">My ticket</p>
             <p className="text-gray-400">Cinemas</p>
           </section>
           <section className="left3">
-            <h1 className="pb-3.5 pt-11">Our Sponsor</h1>
+            <h1 className="pb-3.5 pt-11 max-lg:pt-3.5 font-bold">Our Sponsor</h1>
             <img src="/src/public/ebv.id 2.png" alt="" className="h-10 pb-5" />
             <img src="/src/public/CineOne21 2.png" alt="" className="pb-5" />
             <img src="/src/public/hiflix 2.png" alt="" className="pb-5" />
           </section>
           <section className="left4">
-            <h1 className="pb-2 pl-1 mt-1">Follow us</h1>
+            <h1 className="pb-2 pl-1 mt-1 font-bold">Follow us</h1>
             <article className="flex gap-1">
               <img src="/src/public/eva_facebook-outline.png" alt="" />
               <p className="text-gray-400">Tickitz Cinema id</p>
