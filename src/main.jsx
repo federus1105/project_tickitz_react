99328893167e068f4318b2bb28d9383f8314c174
federus rudi <federusrudi@gmail.com>
2025-08-10 17:01:32 +0700

    feat: main.jsx sebagai komponen utama yang membungkus komponen Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-// import Register from './components/Register.jsx'
-import Login from './components/Login.jsx'
-// import Home_2 from './components/Home_2.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from './App.jsx'
+import Register from "./auth/Register.jsx";
+import Login from "./auth/Login.jsx";
+import List from "./movies/ListMovie.jsx";
+import Router from "./Router.jsx";
+import Payment_Modal from "./movies/Payment_Modal.jsx";
+import DetailMovie from "./movies/Detail-Movie.jsx";
+import Index from "./movies/Index.jsx";
+import ProfilePage from "./movies/ProfilePage.jsx";
+import TicketResult from "./movies/TicketResult.jsx";
+import OrderPage from "./movies/OrderPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-     {/* <Register/> */}
-     <Login/>
-     {/* <Home_2/> */}
-  </StrictMode>,
-)
+    <Router />
+  </StrictMode>
+);
