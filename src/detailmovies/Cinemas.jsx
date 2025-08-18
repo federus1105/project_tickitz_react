77899d2a5948faba1b4eab2863c9d19fb3f2ec7b
federus rudi <federusrudi@gmail.com>
2025-08-18 18:10:10 +0700

    fix: directory images and function redux

--- src/detailmovies/Cinemas.jsx
+++ src/detailmovies/Cinemas.jsx
@@ -1,25 +1,17 @@
-import { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCinema } from "../redux/slice/orderSlice";
 
 const cinemas = [
-  { name: "EBV.id", img: "/src/public/ebv.id.png" },
-  { name: "Hiflix", img: "/src/public/hiflix 2.png" },
-  { name: "CineOne21", img: "/src/public/cineone.png" },
-  { name: "Cinepolis", img: "/src/public/cinepolis.png" },
+  { name: "EBV.id", img: "/ebv.id.png" },
+  { name: "Hiflix", img: "/hiflix 2.png" },
+  { name: "CineOne21", img: "/cineone.png" },
+  { name: "Cinepolis", img: "/cinepolis.png" },
 ];
 
 export default function Cinemas() {
   const dispatch = useDispatch();
   const selectedCinema = useSelector((state) => state.order.cinema);
-  const [showPopup, setShowPopup] = useState(false);
-  // const [selectedCinema, setSelectedCinema] = useState("");
-  const handleContinuw = () => {
-    if (!selectedCinema) {
-      setShowPopup(true);
-      return;
-    }
-  };
   return (
     <section>
       <div className="flex gap-8 mt-10 items-center">
@@ -31,22 +23,22 @@ export default function Cinemas() {
         {cinemas.map((cinema, index) => (
           <label
             key={index}
-            className={`border border-gray-400 py-5 px-5 rounded-sm flex justify-center items-center cursor-pointer transition-all duration-500 ${
-              selectedCinema === cinema.name ? "bg-blue-700" : "bg-white"
+            className={`border border-gray-400 py-5 px-5 max-lg:h-20 rounded-sm flex justify-center items-center cursor-pointer transition-all duration-500 ${
+              selectedCinema?.name === cinema.name ? "bg-blue-700" : "bg-white"
             }`}
           >
             <input
               type="radio"
               name="cinema"
-              value={cinema.name}
+              value={cinema}
               checked={selectedCinema?.name === cinema.name}
-              onChange={() => dispatch(setCinema(cinema.name))}
+              onChange={() => dispatch(setCinema(cinema))}
               className="hidden"
             />
             <img
               src={cinema.img}
               alt={cinema.name}
-              className="w-60 h-auto pointer-events-none"
+              className="w-60 h-auto pointer-events-none max-lg:w-40 max-lg:h-auto"
             />
           </label>
         ))}
