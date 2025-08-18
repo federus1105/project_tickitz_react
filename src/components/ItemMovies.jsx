ac252113aefe347736db57f8838246f6b841ea55
federus rudi <federusrudi@gmail.com>
2025-08-18 18:03:10 +0700

    fix: change the api key variable, to use .env

--- src/components/ItemMovies.jsx
+++ src/components/ItemMovies.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-const API_KEY = "44069dd5e3a0de5d4d7ff0643cb336a0";
 import { Link } from "react-router-dom";
 
 function reverseids(ids, genre) {
@@ -9,7 +8,7 @@ function reverseids(ids, genre) {
   });
 }
 
-function ItemMovies({ selectedGenre, searchTerm, page }) {
+function ItemMovies({ selectedGenres, searchTerm, page }) {
   const [movies, setMovies] = useState([]);
   const [genres, setGenres] = useState([]);
 
@@ -18,26 +17,31 @@ function ItemMovies({ selectedGenre, searchTerm, page }) {
       try {
         // Ambil data genre
         const genreRes = await fetch(
-          `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
+          `https://api.themoviedb.org/3/genre/movie/list?api_key=${
+            import.meta.env.VITE_API_KEY
+          }`
         );
         const genreData = await genreRes.json();
         setGenres(genreData.genres);
 
         let url = "";
         if (searchTerm) {
-          url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${page}`;
+          url = `https://api.themoviedb.org/3/search/movie?api_key=${
+            import.meta.env.VITE_API_KEY
+          }&query=${encodeURIComponent(searchTerm)}&page=${page}`;
         } else {
-          url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${page}`;
-          if (selectedGenre) {
-            url += `&with_genres=${selectedGenre}`;
+          url = `https://api.themoviedb.org/3/discover/movie?api_key=${
+            import.meta.env.VITE_API_KEY
+          }&page=${page}`;
+          if (selectedGenres.length > 0) {
+            url += `&with_genres=${selectedGenres.join(",")}`;
           }
         }
 
         const movieRes = await fetch(url);
         const movieData = await movieRes.json();
-        const results = movieData.results;
 
-        const formatted = results.map((movie) => ({
+        const formatted = (movieData.results || []).map((movie) => ({
           id: movie.id,
           gambar: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
           judul: movie.title,
@@ -52,24 +56,11 @@ function ItemMovies({ selectedGenre, searchTerm, page }) {
     }
 
     fetchData();
-  }, [selectedGenre, searchTerm, page]);
-
-  // Filter movie sesuai selectedGenre dan searchTerm
-  const filteredMovies = movies.filter((movie) => {
-    const matchGenre = selectedGenre
-      ? movie.genre_ids.includes(selectedGenre)
-      : true;
-
-    const matchSearch = searchTerm
-      ? movie.judul.toLowerCase().includes(searchTerm.toLowerCase())
-      : true;
-
-    return matchGenre && matchSearch;
-  });
+  }, [selectedGenres, searchTerm, page]);
 
   return (
-    <section id="three_image" className="grid grid-cols-4 gap-10 mb-11 max-lg:grid-cols-2">
-      {filteredMovies.map((film) => (
+    <section className="grid grid-cols-4 gap-10 mb-11 max-lg:grid-cols-2">
+      {movies.map((film) => (
         <article className="rounded-lg shadow-xl p-4" key={film.id}>
           <Link to={`../detailmovies/${film.id}`}>
             <img
@@ -79,8 +70,8 @@ function ItemMovies({ selectedGenre, searchTerm, page }) {
               className="rounded-lg hover:scale-104 transition-transform duration-300 cursor-pointer"
             />
           </Link>
-          <h1 className="pt-1.5">{film.judul}</h1>
-          <div id="genre" className="text-[12px] text-gray-500">
+          <h1 className="pt-1.5 font-medium">{film.judul}</h1>
+          <div className="text-[12px] text-gray-500">
             {film.genre.map((g, idx) => (
               <button
                 key={idx}
