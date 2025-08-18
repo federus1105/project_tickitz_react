28ec35eac29dedda84c1ef0f53d327c12b61c7c8
federus rudi <federusrudi@gmail.com>
2025-08-18 23:11:35 +0700

    feat: responsive styling for 425px/mobile L

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,32 +3,31 @@ import React from "react";
 function Subscribe() {
   return (
     <>
-      <section
-        id="content5"
-        className="flex flex-row justify-center bg-[#2563eb] h-68 rounded-2xl mt-15 overflow-hidden"
-      >
-        <section>
-          <h1 className="text-[50px] text-white pt-10 text-center max-lg:text-4xl max-lg:mb-10">
+      <section className="flex justify-center bg-[#2563eb] h-[13rem] lg:h-[17rem] md:h-[17rem] rounded-2xl mt-[3.75rem] overflow-hidden relative">
+        <div className="text-center">
+          <h1 className="text-[16px] text-white pt-5 lg:text-5xl lg:mb-10 md:text-3xl">
             Subscribe to our newsletter
           </h1>
-          <input
-            type="text"
-            placeholder="First Name"
-            className="h-11 bg-[#2563eb] border-1 border-white px-8 rounded-lg placeholder:text-white mr-1.5 max-lg:px-2  max-lg:w-40"
-          />
-          <input
-            type="email"
-            placeholder="Email Address"
-            className="h-11 bg-[#2563eb] border-1   border-white px-8 rounded-lg placeholder:text-white max-lg:px-2 max-lg:w-40"
-          />
-          <button className="cursor-pointer h-11 bg-white text-blue-600 rounded-lg px-12 ml-1.5 max-lg:px-3">
-            Subcribe Now
-          </button>
+          <form className="flex flex-wrap justify-center gap-1.5 md:gap-2 lg:gap-4 mt-4 md:flex-col lg:flex-row">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="h-8 w-48 bg-[#2563eb] border border-white px-8 rounded-lg placeholder:text-white placeholder:text-xs placeholder:lg:text-base placeholder:md:text-base lg:px-2 lg:w-[16.25rem] lg:h-13 md:h-13 md:w-[22.5rem]"
+            />
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="h-8 w-48 bg-[#2563eb] border border-white px-8 rounded-lg placeholder:text-white placeholder:text-xs placeholder:lg:text-base placeholder:md:text-base lg:px-2 lg:w-[16.25rem] lg:h-13 md:h-13 md:w-[22.5rem]"
+            />
+            <button className="text-xs lg:text-base md:text-base cursor-pointer h-8 w-48 bg-white text-blue-600 rounded-lg px-12 lg:px-3 lg:h-13 md:h-13 md:w-[22.5rem] lg:w-40">
+              Subscribe Now
+            </button>
+          </form>
           <div
             id="bulat"
-            className="w-55 h-55 border-7 border-white rounded-full absolute right-0 max-lg:mt-12  max-lg:w-35 max-lg:h-35 max-lg:mr-13"
+            className="w-[100px] h-[100px] border-[5px] border-white rounded-full absolute right-0 mt-[-0.5rem] mr-[-2.5rem] lg:mt-10 lg:w-[140px] lg:h-[140px] lg:mr-[-4rem] md:w-[120px] md:h-[120px] md:mt-[-2rem] md:mr-[-3rem]"
           ></div>
-        </section>
+        </div>
       </section>
     </>
   );
