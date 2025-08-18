5cfe58e10f88c6b3878a5757fa9a0a493747dd5f
federus rudi <federusrudi@gmail.com>
2025-08-18 19:01:42 +0700

    feat: usestate function, functions to select 2 components

--- src/movies/ProfilePage.jsx
+++ src/movies/ProfilePage.jsx
@@ -6,23 +6,21 @@ import Account from "../profilpage/Account";
 import OrderHistory from "../profilpage/OrderHistory";
 
 function ProfilePage() {
-  const [Choose, SetChoose] = useState('settings')
+  const [Choose, SetChoose] = useState("settings");
   return (
     <>
-      <body className="bg-[#dedede] ">
+      <div className="bg-[#dedede] min-h-screen w-full">
         <main className="mt-17 mr-28 ml-28 max-lg:mx-18.5">
           <div id="content" className="flex gap-8 max-lg:block">
             <Profile />
             <section>
               <SettingProfile Choose={Choose} SetChoose={SetChoose} />
-
               {Choose === "settings" && <DetailProfil />}
               {Choose === "order" && <OrderHistory />}
-
             </section>
           </div>
         </main>
-      </body>
+      </div>
     </>
   );
 }
