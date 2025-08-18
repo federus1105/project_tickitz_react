5139c30cb823002a50ded3aef2825e96a170af47
federus rudi <federusrudi@gmail.com>
2025-08-18 19:03:10 +0700

    feat: useselector function to retrieve data in store

--- src/movies/TicketResult.jsx
+++ src/movies/TicketResult.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectTotalPrice } from "../redux/slice/orderSlice";
 
 function TicketResult() {
+  const seatPrice = useSelector(selectTotalPrice);
+  const movie = useSelector((state) => state.order.selectedMovie);
+  const { date, time, selectedSeat } = useSelector(
+    (state) => state.order.bookingInfo
+  );
+
   return (
     <>
       <body className="bg-gray-200">
@@ -8,14 +17,14 @@ function TicketResult() {
           <div className="flex gap-35 max-lg:flex max-lg:flex-wrap max-lg:gap-0">
             <div className="relative">
               <img
-                src="/src/public/image 1.svg"
+                src="/image 1.svg"
                 alt=""
                 className="brightness-[25%] w-full h-auto object-cover"
               />
               <div className="absolute top-1/2 left-[9%] transform -translate-y-1/2 text-white text-[25px]">
                 <div>
                   <img
-                    src="/src/public/tickitz 1.svg"
+                    src="/tickitz 1.svg"
                     alt=""
                     className="mb-4 brightness-100"
                   />
@@ -37,56 +46,61 @@ function TicketResult() {
               <div className="absolute w-10 h-10 bg-gray-200 rounded-full top-51 -right-5"></div>
               <div className="mx-8 my-5">
                 <div className="mx-3 pb-10">
-                  <img src="/src/public/QR Code 1.svg" alt="" />
+                  <img src="/QR Code 1.svg" alt="" />
                 </div>
 
-                <div className="flex gap-[35px] pb-3 pt-10">
+                <div className="grid grid-cols-2 pb-3 pt-10">
                   <div>
                     <p className="text-gray-500">Movie</p>
-                    <p>Spider-Man: ..</p>
+                    <p>{movie.title.substring(0, 10)}...</p>
                   </div>
-                  <div>
+                  <div className="flex flex-col items-end">
                     <p className="text-gray-500">Category</p>
                     <p>PG-13</p>
                   </div>
                 </div>
 
-                <div className="flex gap-[91px] pb-3">
+                <div className="grid grid-cols-2 pb-3">
                   <div>
                     <p className="text-gray-500">Date</p>
-                    <p>07 Jul</p>
+                    <p>
+                      {new Date(date).toLocaleDateString("en-GB", {
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </p>
                   </div>
-                  <div>
+                  <div className="flex flex-col items-end">
                     <p className="text-gray-500">Time</p>
-                    <p>2:00pm</p>
+                    <p>{time}</p>
                   </div>
                 </div>
 
-                <div className="flex gap-[89px]">
+                <div className="grid grid-cols-2">
                   <div>
                     <p className="text-gray-500">Count</p>
-                    <p>3 pcs</p>
+                    <p>{selectedSeat.length} Pcs</p>
                   </div>
-                  <div>
+                  <div className="flex flex-col items-end">
                     <p className="text-gray-500">Seats</p>
-                    <p>C4, C5, C6</p>
+                    <p>{selectedSeat.join(", ")}</p>
                   </div>
                 </div>
 
                 <button className="mt-10 w-full h-10 bg-white border border-gray-400 rounded-md flex  justify-between items-center px-5">
                   <p className="text-lg">Total</p>
-                  <p className="font-bold">$30.00</p>
+                  <p className="font-bold">${seatPrice}</p>
                 </button>
               </div>
 
               <div className="flex flex-col absolute -right-5.5">
                 <button className="border border-blue-700 text-blue-700 h-[45px] px-[63px] rounded mt-[45px]">
-                  <img src="/src/public/Frame 94.svg" alt="" />
+                  <img src="/Frame 94.svg" alt="" />
                 </button>
                 <button className="bg-blue-700 border border-blue-700 text-white h-[45px] px-[136px] rounded mt-[10px]">
-                  <p className="text-white">
-                    Done
-                  </p>
+                  <Link to="/movies">
+                    <p className="text-white">Done</p>
+                  </Link>
                 </button>
               </div>
             </div>
