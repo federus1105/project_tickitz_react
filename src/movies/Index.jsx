284c3d71e0d26c2163f218e56662f873066bceb5
federus rudi <federusrudi@gmail.com>
2025-08-18 18:55:22 +0700

    feat: The scroll function on the button works on the itemmovies component

--- src/movies/Index.jsx
+++ src/movies/Index.jsx
@@ -4,8 +4,24 @@ import Subscribe from "../components/Subscribe";
 import Header from "../LandingPage/Header";
 import Testimoni from "../LandingPage/Testimoni";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 function Index() {
+  const scrollRef = useRef(null);
+
+  // untuk scroll
+  const scrollRight = () => {
+    scrollRef.current?.scrollBy({
+      left: 285,
+      behavior: "smooth",
+    });
+  };
+  const scrollLeft = () => {
+    scrollRef.current?.scrollBy({
+      left: -295,
+      behavior: "smooth",
+    });
+  };
   return (
     <>
       <main className="ml-28 mr-28 max-md:mx-5">
@@ -13,8 +29,10 @@ function Index() {
         <Testimoni />
         <section className="pt-15">
           <section className="">
-            <h1 className="text-center text-blue-700 font-bold pb-3">MOVIES</h1>
-            <p className="text-center pb-10 text-3xl">
+            <h1 className="text-center text-blue-700 font-bold pb-3 max-lg:text-sm">
+              MOVIES
+            </h1>
+            <p className="text-center pb-10 text-3xl max-lg:text-2xl">
               Exciting Movies That Should Be
               <br />
               Watched Today
@@ -23,29 +41,44 @@ function Index() {
           <ItemMovies />
           <Link
             to={`./listmovies`}
-            className="flex justify-center my-15 text-blue-700"
+            className="flex justify-center my-15 max-lg:text-md"
           >
-            View All
+            View All{" "}
+            <img
+              src="/icons8-right-24.png"
+              alt=""
+              className="ml-2 max-lg:w-5"
+            />
           </Link>
         </section>
 
-        <section>
-          <h2 className="text-blue-700 font-bold max-lg:text-center">
-            UPCOMING MOVIES
-          </h2>
-          <div className="flex justify-between max-lg:flex max-lg:flex-col max-lg:items-center">
-            <h1 className="text-2xl mb-4">Exciting Movie Coming Soon</h1>
-            <div className="four_headbtn mb-5">
-              <button className="bg-gray-400 w-15 h-15 rounded-full text-white cursor-pointer">
-                -
-              </button>
-              <button className="bg-blue-700 w-15 h-15 rounded-full text-white cursor-pointer ml-2">
-                -
-              </button>
-            </div>
+        <section className="flex justify-between">
+          <button
+            onClick={scrollLeft}
+            className="flex justify-center items-center bg-gray-200 w-15 h-15 rounded-full text-white cursor-pointer"
+          >
+            <img
+              src="/icons8-right-24.png"
+              alt=""
+              className="transform rotate-180"
+            />
+          </button>
+          <div>
+            <h2 className="text-blue-700 font-bold text-center max-lg:text-center max-lg:text-sm">
+              UPCOMING MOVIES
+            </h2>
+            <h1 className="text-2xl mb-4 max-lg:text-xl">
+              Exciting Movie Coming Soon
+            </h1>
           </div>
-          <ItemMovies />
+          <button
+            onClick={scrollRight}
+            className="flex justify-center items-center bg-gray-200 w-15 h-15 rounded-full text-white cursor-pointer"
+          >
+            <img src="/icons8-right-24.png" alt="" />
+          </button>
         </section>
+        <ItemMovies ref={scrollRef} />
         <Subscribe />
       </main>
     </>
